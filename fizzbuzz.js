caeab1c0c0e32be2c206b18c99f886ea470af79a
Julio Protzek <julio@flama.me>
2016-06-04 17:20:06 -0300

    returns fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -17,10 +17,16 @@ describe('FizzBuzz', () => {
     assert.equal('buzz', fizzBuzz(5));
   });
 
+  it('returns fizzbuzz', () => {
+    assert.equal('fizzbuzz', fizzBuzz(15));
+  });
 });
 
 function fizzBuzz(value) {
-  if (value % 3 == 0) {
+  if (value % 3 == 0 && value % 5 == 0) {
+    return "fizzbuzz";    
+  }
+  else if (value % 3 == 0) {
     return "fizz";
   }else if (value % 5 == 0) {
     return "buzz";
