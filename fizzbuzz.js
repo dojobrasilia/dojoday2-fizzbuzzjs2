d115bd7174fde79d8880417af81883a25f9694a9
Julio Protzek <julio@flama.me>
2016-06-04 17:08:43 -0300

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+// function hello(name) {
+//   return 'Hello ' + name;
+// }
+//
+// describe('FizzBuzz', () => {
+//   it('greets the user', () => {
+//     assert.equal('Hello World', hello('Julio'));
+//   });
+// });
+
+// fizzbuzz(1) => 1
+// fizzbuzz(2) => 2
+// fizzbuzz(3) => Fizz
+// fizzbuzz(5) => Buzz
+// fizzbuzz(15) => FizzBuzz
